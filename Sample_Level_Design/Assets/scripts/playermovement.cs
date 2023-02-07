628fb39abf556bc75f86d87c6c283c94b3330415
SunhikPramanik <46312401+SunhikPramanik@users.noreply.github.com>
2023-02-07 19:47:58 +0530

    pushing

--- Sample_Level_Design/Assets/scripts/playermovement.cs
+++ Sample_Level_Design/Assets/scripts/playermovement.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class playermovement : MonoBehaviour
 {
-    public float mov_speed;
+    public float mov_speed=2;
     public float jump_speed;
     private Vector3 dir;
     private Rigidbody rb;
     public bool onGround=true;
+    public bool flag = false;
     // Start is called before the first frame update
+
+    public static playermovement instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
     void Start()
     {
-        dir = Vector3.right;
+        dir = Vector3.forward;
         rb=GetComponent<Rigidbody>();
     }
 
@@ -22,49 +32,53 @@ public class playermovement : MonoBehaviour
     {
         if(this.transform.position.y<-1)
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        /*this.transform.Translate(dir * mov_speed * Time.deltaTime);
-        if ((dir == Vector3.right) && Input.GetKeyDown(KeyCode.D))
-        {
-            dir = Vector3.back;
-        }
-        if ((dir == Vector3.forward)&&Input.GetKeyDown(KeyCode.D))
+        this.transform.Translate(dir * mov_speed * Time.deltaTime);
+        if ((dir == Vector3.forward) && Input.GetKeyDown(KeyCode.D))
         {
             dir = Vector3.right;
         }
-        if ((dir == Vector3.right) && Input.GetKeyDown(KeyCode.A))
+        if ((dir == Vector3.left)&&Input.GetKeyDown(KeyCode.D) )
         {
             dir = Vector3.forward;
         }
-        if ((dir == Vector3.back) && Input.GetKeyDown(KeyCode.A))
+        if ((dir == Vector3.forward) && Input.GetKeyDown(KeyCode.A) )
         {
-            dir = Vector3.right;
-        }*/
-        if(Input.GetKey(KeyCode.A))
-        {
-            this.transform.Translate(Vector3.forward * mov_speed * Time.deltaTime);
+            dir = Vector3.left;
         }
-        if (Input.GetKey(KeyCode.D))
+        if ((dir == Vector3.right) && Input.GetKeyDown(KeyCode.A) )
         {
-            this.transform.Translate(Vector3.back * mov_speed * Time.deltaTime);
+            dir = Vector3.forward;
         }
-        if (Input.GetKey(KeyCode.S))
+        /*if(Input.GetKey(KeyCode.A))
         {
             this.transform.Translate(Vector3.left * mov_speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.D))
         {
             this.transform.Translate(Vector3.right * mov_speed * Time.deltaTime);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back * mov_speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * mov_speed * Time.deltaTime);
+        }*/
         if (Input.GetKeyDown(KeyCode.Space) && onGround==true)
         {
-            rb.AddForce(Vector3.up * jump_speed, ForceMode.Impulse);
-            onGround= false;
+            flag = true;
         }
-        if (this.transform.position.x > 39)
+    }
+    private void FixedUpdate()
+    {
+        if (flag == true)
         {
-            SceneManager.LoadScene(1);
+            rb.AddForce(Vector3.up * jump_speed, ForceMode.Impulse);
+            onGround = false;
+            flag= false;
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -75,7 +89,18 @@ public class playermovement : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Enemy") == true)
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+        if(collision.gameObject.CompareTag("Slippery")==true)
+        {
+            onGround = true;
+            mov_speed = 8;
+        } else mov_speed= 2;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("LevelChange")==true)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
